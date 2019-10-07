78db5c92598b8d30d46949c7e0916ce696ef9d61
berny2411 <beregaladocabrera@gmail.com>
2019-10-07 13:10:25 -0500

    no message

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('HOMEDIR',__DIR__);
-	include 'views/header.php';
-  	$page=isset($_GET['page'])?$_GET['page']:'login';
-	include 'views/'.$page.'.php';
-?>
--- js/medAseguradora.js
+++
@@ -1,6 +0,0 @@
-$('#clonform').click(function() {
-  var clone = $('#alta_MedicoAs').clone('#alta_MedicoAs');
-  $('#formO').append(clone);
-
-});
-
